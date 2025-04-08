ccef5784015989f3f21234b0d20ac35748ea585c
bhhvshh <22bce377@nirmauni.ac.in>
2025-04-08 08:42:43 +0530

    atlas connected-working properly

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongo = async  () =>
     {
-           const res = await mongoose.connect("mongodb://localhost:27017/blog-mern");
+           const res = await mongoose.connect("mongodb+srv://bhaveshvaniya102:SecureWeb%4020@secureweb.h1aho5k.mongodb.net/myappdb?retryWrites=true&w=majority&appName=secureweb");
            if(res)
             {
                 console.log("Connected successfully");
